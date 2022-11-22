49c7437d661a4121be65fff6ac7fb348d3f01065
Brent Ely <gitbrent@users.noreply.github.com>
2022-11-21 23:38:22 -0600

    image grid working

--- src/AppMain.tsx
+++ src/AppMain.tsx
@@ -1,4 +1,5 @@
 /**
+ * @see https://developers.google.com/drive/api/guides/about-sdk
  * @see https://developers.google.com/drive/api/guides/search-files#node.js
  * @see https://medium.com/@willikay11/how-to-link-your-react-application-with-google-drive-api-v3-list-and-search-files-2e4e036291b7
  */
@@ -71,13 +72,21 @@ interface IGapiDoc {
 	size?: string
 }
 
+enum FileIcons {
+	'application/vnd.google-apps.folder' = 'folder',
+	'application/vnd.google-apps.document' = 'doc',
+	'application/vnd.google-apps.presentation' = 'ppt',
+	'application/json' = 'json',
+	'text/plain' = "txt",
+	'image/png' = "png"
+}
+
 export default function AppMain() {
 	const GAPI_CLIENT_ID = process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID;
 	const GAPI_API_KEY = process.env.REACT_APP_GOOGLE_DRIVE_API_KEY;
 	const GAPI_DISC_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
 	const GAPI_SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.readonly';
 
-	const [listDocumentsVisible, setListDocumentsVisibility] = useState(false);
 	const [documents, setDocuments] = useState<IGapiDoc[]>([]);
 	const [isLoadingGoogleDriveApi, setIsLoadingGoogleDriveApi] = useState(false);
 	const [isFetchingGoogleDriveFiles, setIsFetchingGoogleDriveFiles] = useState(false);
@@ -89,6 +98,13 @@ export default function AppMain() {
 	 */
 	const handleAuthClick = () => {
 		gapi.auth2.getAuthInstance().signIn();
+	};
+
+	/**
+  *  Sign out the user upon button click.
+  */
+	const handleSignOutClick = (event) => {
+		gapi.auth2.getAuthInstance().signOut();
 	};
 
 	/**
@@ -148,49 +164,70 @@ export default function AppMain() {
 		setIsFetchingGoogleDriveFiles(true);
 		gapi.client.drive.files
 			.list({
-				pageSize: 10,
-				fields: 'nextPageToken, files(id,name,createdTime,mimeType,modifiedTime,size)',
-				q: searchTerm,
+				pageSize: 15,
+				fields: 'nextPageToken, files(id, name, createdTime, mimeType, modifiedTime, size)',
+				//q: `${searchTerm} mimeType = 'image/png'`,
+				//q: `mimeType = 'image/png'`,
+				q: `mimeType = 'image/png' or mimeType = 'image/jpeg'`,
 			})
 			.then(function(response) {
 				setIsFetchingGoogleDriveFiles(false);
-				setListDocumentsVisibility(true);
 				const res = JSON.parse(response.body);
 				setDocuments(res.files);
 				console.log(res.files);
 			});
 	};
 
+	function renderTable(): JSX.Element {
+		return (<div className='p-4 bg-dark'>
+			<table className='table table-sm mb-0'>
+				<thead>
+					<tr>
+						<th>Type</th>
+						<th>Filename</th>
+						<th>Created</th>
+						<th>Modified</th>
+						<th className='text-end'>Size (MB)</th>
+					</tr>
+				</thead>
+				<tbody>
+					{documents.sort((a, b) => a.name < b.name ? -1 : 1).map((doc) =>
+						<tr key={doc.id}>
+							<td>{FileIcons[doc.mimeType]}</td>
+							<th>{doc.name}</th>
+							<td>{new Date(doc.createdTime).toLocaleString()}</td>
+							<td>{new Date(doc.modifiedTime).toLocaleString()}</td>
+							<td className='text-end'>{doc.size ? (Number(doc.size) / 1024 / 1024).toFixed(1) : ''}</td>
+							<td>{doc.mimeType.indexOf('image') > -1 && <img src={`https://drive.google.com/uc?export=view&id=${doc.id}`} alt="img" />}</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
+		</div>)
+	}
+
 	return (
-		<main className='text-center'>
+		<main>
 			{isLoadingGoogleDriveApi && <div>LOADING</div>}
 			{signedInUser ? <section>
-				<h5>Welcome {signedInUser}</h5>
-				<table className='table table-sm mb-0'>
-					<thead>
-						<tr>
-							<th>Filename</th>
-							<th>Created</th>
-							<th>Modified</th>
-							<th>Size (MB)</th>
-						</tr>
-					</thead>
-					<tbody>
-						{documents.map((doc) =>
-							<tr>
-								<td>{doc.name}</td>
-								<td>{new Date(doc.createdTime).toLocaleString()}</td>
-								<td>{new Date(doc.modifiedTime).toLocaleString()}</td>
-								<td>{doc.size ? (Number(doc.size) / 1024 / 1024).toFixed(1) : ''}</td>
-							</tr>
+				<div className='row align-items-center mb-4'>
+					<div className='col'><h5 className='mb-0'>Welcome {signedInUser}</h5></div>
+					<div className='col-auto'><button className='btn btn-primary' type='button' onClick={handleSignOutClick}>Logout</button></div>
+				</div>
+				<div className='p-4 bg-dark'>
+					<div className='row row-cols-1 row-cols-md-2 row-cols-lg-4 justify-content-between align-items-center g-4'>
+						{documents.sort((a, b) => a.modifiedTime < b.modifiedTime ? -1 : 1).map((doc) =>
+							<div className='col'>
+								<img src={`https://drive.google.com/uc?export=view&id=${doc.id}`} alt={`${doc.name}`} style={{ width: '100%', height: '100%' }} />
+							</div>
 						)}
-					</tbody>
-				</table>
+					</div>
+				</div>
 			</section> :
-				<section onClick={() => handleClientLoad()} className="p-4 bg-dark">
+				<section onClick={() => handleClientLoad()} className="text-center p-4 bg-dark">
 					<img height="100" width="100" src="https://raw.githubusercontent.com/willikay11/React-Google-Drive-Tutorial/master/src/assets/images/google-drive.png" alt="GoogleDriveImage" />
 					<h5>Google Drive</h5>
-					<span>view media straight from your google drive</span>
+					<span>view media directly from your google drive</span>
 				</section>
 			}
 		</main >
