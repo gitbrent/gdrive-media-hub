42d1871a59f1cc9a614b888357a1939acab6974a
Brent Ely <gitbrent@users.noreply.github.com>
2022-11-22 22:42:06 -0600

    FIXED - app works local and deployed now

--- src/AppMain.tsx
+++ src/AppMain.tsx
@@ -1,9 +1,10 @@
 /**
  * @see https://developers.google.com/drive/api/guides/about-sdk
  * @see https://developers.google.com/drive/api/guides/search-files#node.js
+ * @see https://developers.google.com/drive/api/v3/reference/files/get
  * @see https://medium.com/@willikay11/how-to-link-your-react-application-with-google-drive-api-v3-list-and-search-files-2e4e036291b7
  */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { gapi } from 'gapi-script';
 
 interface IGapiCurrUser {
@@ -38,7 +39,7 @@ interface IGapiCurrUser {
 	},
 }
 
-interface IGapiDoc {
+interface IGapiFile {
 	/**
 	 * id
 	 * @example "1l5mVFTysjVoZ14_unp5F8F3tLH7Vkbtc"
@@ -70,15 +71,8 @@ interface IGapiDoc {
 	 * @example "3516911"
 	 */
 	size?: string
-}
-
-enum FileIcons {
-	'application/vnd.google-apps.folder' = 'folder',
-	'application/vnd.google-apps.document' = 'doc',
-	'application/vnd.google-apps.presentation' = 'ppt',
-	'application/json' = 'json',
-	'text/plain' = "txt",
-	'image/png' = "png"
+	// WORKS
+	imageBlobUrl: string
 }
 
 export default function AppMain() {
@@ -86,12 +80,19 @@ export default function AppMain() {
 	const GAPI_API_KEY = process.env.REACT_APP_GOOGLE_DRIVE_API_KEY;
 	const GAPI_DISC_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
 	const GAPI_SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.readonly';
-
-	const [documents, setDocuments] = useState<IGapiDoc[]>([]);
+	//
+	const PAGE_SIZE = 12
+	//
+	const [signedInUser, setSignedInUser] = useState('');
+	const [gapiFiles, setGapiFiles] = useState<IGapiFile[]>([]);
 	const [isLoadingGoogleDriveApi, setIsLoadingGoogleDriveApi] = useState(false);
 	const [isFetchingGoogleDriveFiles, setIsFetchingGoogleDriveFiles] = useState(false);
-	const [signedInUser, setSignedInUser] = useState('');
-	//const handleChange = (file) => { };
+
+	/** fetch images */
+	useEffect(() => {
+		gapiFiles.forEach((file: IGapiFile) => { downloadFile(file.id) });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [gapiFiles])
 
 	/**
 	 *  Sign in the user upon button click.
@@ -101,8 +102,8 @@ export default function AppMain() {
 	};
 
 	/**
-  *  Sign out the user upon button click.
-  */
+	 *  Sign out the user upon button click.
+	 */
 	const handleSignOutClick = (event) => {
 		gapi.auth2.getAuthInstance().signOut();
 	};
@@ -164,47 +165,31 @@ export default function AppMain() {
 		setIsFetchingGoogleDriveFiles(true);
 		gapi.client.drive.files
 			.list({
-				pageSize: 15,
+				pageSize: PAGE_SIZE,
 				fields: 'nextPageToken, files(id, name, createdTime, mimeType, modifiedTime, size)',
-				//q: `${searchTerm} mimeType = 'image/png'`,
-				//q: `mimeType = 'image/png'`,
 				q: `mimeType = 'image/png' or mimeType = 'image/jpeg'`,
 			})
 			.then(function(response) {
 				setIsFetchingGoogleDriveFiles(false);
 				const res = JSON.parse(response.body);
-				setDocuments(res.files);
-				console.log(res.files);
+				setGapiFiles(res.files);
 			});
 	};
 
-	function renderTable(): JSX.Element {
-		return (<div className='p-4 bg-dark'>
-			<table className='table table-sm mb-0'>
-				<thead>
-					<tr>
-						<th>Type</th>
-						<th>Filename</th>
-						<th>Created</th>
-						<th>Modified</th>
-						<th className='text-end'>Size (MB)</th>
-					</tr>
-				</thead>
-				<tbody>
-					{documents.sort((a, b) => a.name < b.name ? -1 : 1).map((doc) =>
-						<tr key={doc.id}>
-							<td>{FileIcons[doc.mimeType]}</td>
-							<th>{doc.name}</th>
-							<td>{new Date(doc.createdTime).toLocaleString()}</td>
-							<td>{new Date(doc.modifiedTime).toLocaleString()}</td>
-							<td className='text-end'>{doc.size ? (Number(doc.size) / 1024 / 1024).toFixed(1) : ''}</td>
-							<td>{doc.mimeType.indexOf('image') > -1 && <img src={`https://drive.google.com/uc?export=view&id=${doc.id}`} alt="img" />}</td>
-						</tr>
-					)}
-				</tbody>
-			</table>
-		</div>)
+	const downloadFile = (fileId: string) => {
+		gapi.client.drive.files.get({ fileId: fileId, alt: "media", })
+			.then((response) => {
+				// 1
+				const objectUrl = URL.createObjectURL(new Blob([new Uint8Array(response.body.length).map((_, i) => response.body.charCodeAt(i))], { type: 'image/png' }));
+				// 2
+				const updFiles = gapiFiles
+				updFiles.filter((file) => file.id === fileId)[0].imageBlobUrl = objectUrl;
+				setGapiFiles(updFiles)
+			})
+			.catch((err) => console.log(err))
 	}
+
+	// --------------------------------------------------------------------------------------------
 
 	return (
 		<main>
@@ -214,11 +199,11 @@ export default function AppMain() {
 					<div className='col'><h5 className='mb-0'>Welcome {signedInUser}</h5></div>
 					<div className='col-auto'><button className='btn btn-primary' type='button' onClick={handleSignOutClick}>Logout</button></div>
 				</div>
-				<div className='p-4 bg-dark'>
+				<div className='p-4 bg-dark mt-4'>
 					<div className='row row-cols-1 row-cols-md-2 row-cols-lg-4 justify-content-between align-items-center g-4'>
-						{documents.sort((a, b) => a.modifiedTime < b.modifiedTime ? -1 : 1).map((doc) =>
-							<div className='col'>
-								<img src={`https://drive.google.com/uc?export=view&id=${doc.id}`} alt={`${doc.name}`} style={{ width: '100%', height: '100%' }} />
+						{gapiFiles.sort((a, b) => a.modifiedTime < b.modifiedTime ? -1 : 1).map((file) =>
+							<div key={file.id} className='col'>
+								<img src={file.imageBlobUrl} alt={file.name} style={{ width: '100%', height: '100%' }} />
 							</div>
 						)}
 					</div>
