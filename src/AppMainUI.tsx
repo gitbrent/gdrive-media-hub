ee799010d3d602bf99c91c548f17d436a06e07a4
Brent Ely <gitbrent@users.noreply.github.com>
2023-10-07 13:41:06 -0500

    updated UI

--- src/AppMainUI.tsx
+++ src/AppMainUI.tsx
@@ -20,8 +20,6 @@ export default function AppMainUI() {
 	const [optIsShowCap, setOptIsShowCap] = useState(true)
 	const [optSlideshowSecs, setOptSlideshowSecs] = useState(DEFAULT_SLIDE_DELAY)
 	//
-	const [debugShowFileNames, setDebugShowFileNames] = useState(false)
-	//
 	const [showFiles, setShowFiles] = useState<IGapiFile[]>([])
 	//
 	const [isSidebarOpen, setSidebarOpen] = useState(false)
@@ -89,6 +87,7 @@ export default function AppMainUI() {
 
 	// --------------------------------------------------------------------------------------------
 
+	// TODO: move search to NavBar (or new topbar)
 	function renderNavbar(): JSX.Element {
 		function renderBtns(): JSX.Element {
 			const isDisabledNext = (showFiles.length < pagingSize) || ((pagingPage - 1) * pagingSize + showFiles.length >= allFiles.length)
@@ -219,8 +218,6 @@ export default function AppMainUI() {
 										<li>
 											<hr className="dropdown-divider" />
 										</li>
-										<li><h6 className="dropdown-header">Raw Data</h6></li>
-										<li><button className="dropdown-item" onClick={() => setDebugShowFileNames(!debugShowFileNames)}>{debugShowFileNames ? 'Hide' : 'Show'}&nbsp;file.names</button></li>
 									</ul>
 								</li>
 							}
@@ -271,150 +268,152 @@ export default function AppMainUI() {
 	}
 
 	function renderLogin(): JSX.Element {
-		return (<section onClick={() => handleAuthClick()} className="text-center p-4 bg-dark">
-			<div className='p-4'>
-				<img height="150" width="150" src="/google-drive.png" alt="GoogleDriveImage" />
+		return (<section onClick={() => handleAuthClick()} className="text-center">
+			<div className="p-4">
+				<img height="150" width="150" src="/google-drive.png" alt="GoogleDriveLogo" />
 			</div>
 			<h5>Google Drive</h5>
 			<p>view media directly from your google drive</p>
-		</section >)
+		</section>)
 	}
 
-	function renderMainContent(): JSX.Element {
-		return (
-			isBusyGapiLoad ?
-				<section>
-					{renderLogin()}
-					< div className='text-center bg-dark p-3' >
-						<div className="spinner-border text-primary" role="status"><span className="visually-hidden">Loading...</span></div>
-					</div >
-				</section >
-				:
-				optIsSlideshow ?
-					<ImageSlideshow
-						images={allFiles.filter((item) => { return !optSchWord || item.name.toLowerCase().indexOf(optSchWord.toLowerCase()) > -1 })}
-						duration={optSlideshowSecs}
-						downloadFile={downloadFile}
-					/>
-					:
-					debugShowFileNames ?
-						<section>
-							{allFiles?.map(item => item.name).sort().map((item, idx) => (<div key={`badge${idx}`} className='badge bg-info mb-2 me-2'>[{idx}]&nbsp;{item}</div>))}
-						</section>
-						:
-						<section>
-							{authUserName ? <ImageGrid gapiFiles={showFiles} isShowCap={optIsShowCap} selGridSize={GridSizes[1]} /> : renderLogin()}
-						</section>
-		)
-	}
-
-	// TODO: show actual user
-	function renderContainer(): JSX.Element {
+	function renderMainContNav(): JSX.Element {
 		//const isDisabledNext = (showFiles.length < pagingSize) || ((pagingPage - 1) * pagingSize + showFiles.length >= allFiles.length)
 		//const isSlidePaused = optSlideshowSecs === 999
 
-		return (
-			<div className="container-fluid">
-				<div className="row flex-nowrap">
-					<div className={`col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark ${isSidebarOpen ? '' : 'collapsed'}`}>
-						<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-							<a href="#" onClick={toggleSidebar} className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none" title="collapse/expand">
-								<i className="fs-4 bi-list" /><span className={`ms-2 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Menu</span>
-							</a>
-							<ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-								<li className="nav-item">
-									<a href="#" className="nav-link align-middle px-0">
-										<i className="fs-4 bi-house"></i><span className="ms-2 d-inline">Home</span>
-									</a>
-								</li>
-								<li>
-									<a href="#submenuSortBy" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
-										<i className="fs-4 bi-sort-alpha-down"></i><span className="ms-2 d-inline">Sort By</span>
-									</a>
-									<ul className="collapse nav flex-column" id="submenuSortBy" data-bs-parent="#menu">
-										<li className="w-100">
-											<button className={`dropdown-item ${optSortBy === OPT_SORTBY.modDate_full ? '' : 'text-light'}`} disabled={optSortBy === OPT_SORTBY.modDate_full} onClick={() => setOptSortBy(OPT_SORTBY.modDate_full)}>{isSidebarOpen ? OPT_SORTBY.modDate_full : OPT_SORTBY.modDate_trim}</button>
-											<button className={`dropdown-item ${optSortBy === OPT_SORTBY.filName_full ? '' : 'text-light'}`} disabled={optSortBy === OPT_SORTBY.filName_full} onClick={() => setOptSortBy(OPT_SORTBY.filName_full)}>{isSidebarOpen ? OPT_SORTBY.filName_full : OPT_SORTBY.filName_trim}</button>
-										</li>
-									</ul>
-								</li>
-								<li>
-									<a href="#submenuSortDirection" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
-										<i className="fs-4 bi-arrow-down-up"></i><span className="ms-2 d-inline">Sort Direction</span>
-									</a>
-									<ul className="collapse nav flex-column" id="submenuSortDirection" data-bs-parent="#menu">
-										<li className="w-100">
-											<button className={`dropdown-item ${optSortDir === OPT_SORTDIR.asce_full ? '' : 'text-light'}`} disabled={optSortDir === OPT_SORTDIR.asce_full} onClick={() => setOptSortDir(OPT_SORTDIR.asce_full)}>{isSidebarOpen ? OPT_SORTDIR.asce_full : OPT_SORTDIR.asce_trim}</button>
-											<button className={`dropdown-item ${optSortDir === OPT_SORTDIR.desc_full ? '' : 'text-light'}`} disabled={optSortDir === OPT_SORTDIR.desc_full} onClick={() => setOptSortDir(OPT_SORTDIR.desc_full)}>{isSidebarOpen ? OPT_SORTDIR.desc_full : OPT_SORTDIR.desc_trim}</button>
-										</li>
-									</ul>
-								</li>
-								<li>
-									<a href="#submenuPageSize" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
-										<i className="fs-4 bi-grid"></i><span className="ms-2 d-inline">Page Size</span>
-									</a>
-									<ul className="collapse nav flex-column" id="submenuPageSize" data-bs-parent="#menu">
-										<li className="w-100">
-											<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps08_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps08_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps08_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps08_full : OPT_PAGESIZE.ps08_trim}</button>
-											<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps12_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps12_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps12_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps12_full : OPT_PAGESIZE.ps12_trim}</button>
-											<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps24_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps24_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps24_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps24_full : OPT_PAGESIZE.ps24_trim}</button>
-											<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps48_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps48_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps48_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps48_full : OPT_PAGESIZE.ps48_trim}</button>
-										</li>
-									</ul>
-								</li>
-								<li>
-									<a href="#submenuSlideshow" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
-										<i className="fs-4 bi-play-circle"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Slideshow</span>
-									</a>
-									<ul className="collapse nav flex-column ms-1" id="submenuSlideshow" data-bs-parent="#menu">
-										<li className="w-100">
-											{optIsSlideshow ?
-												<>
-													<button className="dropdown-item" onClick={() => { setOptSlideshowSecs(DEFAULT_SLIDE_DELAY) }}>
-														<i className="bi-play-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Resume</span>
-													</button>
-													<button className="dropdown-item" onClick={() => { setOptSlideshowSecs(999) }}>
-														<i className="bi-pause-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Pause</span>
-													</button>
-													<button className="dropdown-item" onClick={() => { setOptIsSlideshow(false) }}>
-														<i className="bi-stop-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Stop</span>
-													</button>
-												</>
-												:
-												<button className="dropdown-item" onClick={() => { setOptIsSlideshow(true); setOptSlideshowSecs(4) }}>
-													<i className="bi-play-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Start</span>
-												</button>
-											}
-										</li>
-									</ul>
-								</li>
-							</ul>
-							<hr />
-							<div className="dropdown pb-4">
-								<a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-									<img src={authUserPict || undefined} alt="hugenerd" width="30" height="30" className="rounded-circle" />
-									<span className={`mx-1 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>{authUserName}</span>
-								</a>
-								<ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-									<li><a className="dropdown-item" href="#">Settings</a></li>
-									<li><a className="dropdown-item" href="#">Profile</a></li>
-									<li>
-										<hr className="dropdown-divider" />
-									</li>
-									<li>
-										<button className="dropdown-item" disabled={!authUserName} onClick={handleSignOutClick}>Sign Out</button>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div className="col py-3">
-						{renderMainContent()}
-					</div>
+		return (<div className={`col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark ${isSidebarOpen ? '' : 'collapsed'}`}>
+			<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+				<a href="#" onClick={toggleSidebar} className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none" title="collapse/expand">
+					<i className="fs-4 bi-list" /><span className={`ms-2 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Menu</span>
+				</a>
+				<ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+					<li className="nav-item">
+						<a href="#" className="nav-link align-middle px-0">
+							<i className="fs-4 bi-house"></i><span className="ms-2 d-inline">Home</span>
+						</a>
+					</li>
+					<li>
+						<a href="#submenuSortBy" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
+							<i className="fs-4 bi-sort-alpha-down"></i><span className="ms-2 d-inline">Sort By</span>
+						</a>
+						<ul className="collapse nav flex-column" id="submenuSortBy" data-bs-parent="#menu">
+							<li className="w-100">
+								<button className={`dropdown-item ${optSortBy === OPT_SORTBY.modDate_full ? '' : 'text-light'}`} disabled={optSortBy === OPT_SORTBY.modDate_full} onClick={() => setOptSortBy(OPT_SORTBY.modDate_full)}>{isSidebarOpen ? OPT_SORTBY.modDate_full : OPT_SORTBY.modDate_trim}</button>
+								<button className={`dropdown-item ${optSortBy === OPT_SORTBY.filName_full ? '' : 'text-light'}`} disabled={optSortBy === OPT_SORTBY.filName_full} onClick={() => setOptSortBy(OPT_SORTBY.filName_full)}>{isSidebarOpen ? OPT_SORTBY.filName_full : OPT_SORTBY.filName_trim}</button>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#submenuSortDirection" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
+							<i className="fs-4 bi-arrow-down-up"></i><span className="ms-2 d-inline">Sort Direction</span>
+						</a>
+						<ul className="collapse nav flex-column" id="submenuSortDirection" data-bs-parent="#menu">
+							<li className="w-100">
+								<button className={`dropdown-item ${optSortDir === OPT_SORTDIR.asce_full ? '' : 'text-light'}`} disabled={optSortDir === OPT_SORTDIR.asce_full} onClick={() => setOptSortDir(OPT_SORTDIR.asce_full)}>{isSidebarOpen ? OPT_SORTDIR.asce_full : OPT_SORTDIR.asce_trim}</button>
+								<button className={`dropdown-item ${optSortDir === OPT_SORTDIR.desc_full ? '' : 'text-light'}`} disabled={optSortDir === OPT_SORTDIR.desc_full} onClick={() => setOptSortDir(OPT_SORTDIR.desc_full)}>{isSidebarOpen ? OPT_SORTDIR.desc_full : OPT_SORTDIR.desc_trim}</button>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#submenuPageSize" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
+							<i className="fs-4 bi-grid"></i><span className="ms-2 d-inline">Page Size</span>
+						</a>
+						<ul className="collapse nav flex-column" id="submenuPageSize" data-bs-parent="#menu">
+							<li className="w-100">
+								<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps08_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps08_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps08_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps08_full : OPT_PAGESIZE.ps08_trim}</button>
+								<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps12_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps12_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps12_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps12_full : OPT_PAGESIZE.ps12_trim}</button>
+								<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps24_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps24_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps24_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps24_full : OPT_PAGESIZE.ps24_trim}</button>
+								<button className={`dropdown-item ${optPgeSize === OPT_PAGESIZE.ps48_full ? '' : 'text-light'}`} disabled={optPgeSize === OPT_PAGESIZE.ps48_full} onClick={() => setOptPgeSize(OPT_PAGESIZE.ps48_full)}>{isSidebarOpen ? OPT_PAGESIZE.ps48_full : OPT_PAGESIZE.ps48_trim}</button>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#submenuSlideshow" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-nowrap">
+							<i className="fs-4 bi-play-circle"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Slideshow</span>
+						</a>
+						<ul className="collapse nav flex-column ms-1" id="submenuSlideshow" data-bs-parent="#menu">
+							<li className="w-100">
+								{optIsSlideshow ?
+									<>
+										<button className="dropdown-item" onClick={() => { setOptSlideshowSecs(DEFAULT_SLIDE_DELAY) }}>
+											<i className="bi-play-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Resume</span>
+										</button>
+										<button className="dropdown-item" onClick={() => { setOptSlideshowSecs(999) }}>
+											<i className="bi-pause-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Pause</span>
+										</button>
+										<button className="dropdown-item" onClick={() => { setOptIsSlideshow(false) }}>
+											<i className="bi-stop-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Stop</span>
+										</button>
+									</>
+									:
+									<button className="dropdown-item" onClick={() => { setOptIsSlideshow(true); setOptSlideshowSecs(4) }}>
+										<i className="bi-play-fill"></i><span className={`ms-2 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>Start</span>
+									</button>
+								}
+							</li>
+						</ul>
+					</li>
+				</ul>
+				<hr />
+				<div className="dropdown pb-4">
+					<a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+						{authUserPict ? <img src={authUserPict} alt="User Avatar" width="30" height="30" className="rounded-circle" /> : <i className="fs-4 bi bi-question-circle-fill"></i>}
+						<span className={`mx-1 ${isSidebarOpen ? 'd-sm-inline' : 'd-none'}`}>{authUserName}</span>
+					</a>
+					<ul className="dropdown-menu dropdown-menu-dark text-small shadow">
+						<li><a className="dropdown-item" href="#">Settings</a></li>
+						<li><a className="dropdown-item" href="#">Profile</a></li>
+						<li>
+							<hr className="dropdown-divider" />
+						</li>
+						<li>
+							<button className="dropdown-item" disabled={!authUserName} onClick={handleSignOutClick}>Sign Out</button>
+						</li>
+					</ul>
 				</div>
 			</div>
+		</div>
 		)
 	}
 
-	return renderContainer()
+	function renderMainContBody(): JSX.Element {
+		let returnJsx = <div>Loading...</div>
+
+		if (isBusyGapiLoad) {
+			returnJsx = <section className='flex-grow'>
+				{renderLogin()}
+				<div className='text-center p-3'>
+					<div className="spinner-border text-primary" role="status"><span className="visually-hidden">Loading...</span></div>
+				</div>
+			</section>
+		}
+		else if (authUserName) {
+			if (optIsSlideshow) {
+				returnJsx = <ImageSlideshow
+					images={allFiles.filter((item) => { return !optSchWord || item.name.toLowerCase().indexOf(optSchWord.toLowerCase()) > -1 })}
+					duration={optSlideshowSecs}
+					downloadFile={downloadFile}
+				/>
+			}
+			else {
+				returnJsx = <ImageGrid gapiFiles={showFiles} isShowCap={optIsShowCap} selGridSize={GridSizes[1]} />
+			}
+		}
+		else {
+			returnJsx = renderLogin()
+		}
+
+		return (
+			<div className="col py-3">{returnJsx}</div>
+		)
+	}
+
+	return (
+		<div className="container-fluid">
+			<div className="row flex-nowrap">
+				{renderMainContNav()}
+				{renderMainContBody()}
+			</div>
+		</div>
+	)
 }
