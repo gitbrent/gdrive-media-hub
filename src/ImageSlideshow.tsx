307ba537b97610274f9c9a9be5b398501b9634e5
Brent Ely <gitbrent@users.noreply.github.com>
2023-10-23 17:44:41 -0500

    major update: pre-randomized, pre-fetching finally works

--- src/ImageSlideshow.tsx
+++ src/ImageSlideshow.tsx
@@ -7,39 +7,48 @@ interface Props {
 	downloadFile: (fileId: string) => Promise<boolean>;
 }
 
-const Slideshow: React.FC<Props> = ({ images, duration, downloadFile }) => {
+const ImageSlideshow: React.FC<Props> = ({ images, duration, downloadFile }) => {
+	const [shuffledImages, setShuffledImages] = useState<IGapiFile[]>([])
 	const [currentIndex, setCurrentIndex] = useState(0)
+	const [usedIndices, setUsedIndices] = useState<number[]>([])
 
+	// Shuffle images once at the beginning
+	useEffect(() => {
+		const shuffled = [...images].sort(() => Math.random() - 0.5)
+		setShuffledImages(shuffled)
+	}, [images])
+
+	// Slide-show logic
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setCurrentIndex(Math.floor(Math.random() * images.length))
+			if (shuffledImages.length === 0) return
+			const nextIndex = (currentIndex + 1) % shuffledImages.length
+			setCurrentIndex(nextIndex)
+			setUsedIndices([...usedIndices, nextIndex])
 		}, duration * 1000)
 
 		return () => clearInterval(interval)
-	}, [duration, images.length])
+	}, [duration, currentIndex, shuffledImages, usedIndices])
 
-	/**
-	 * Pre-load images
-	 * @description Not all images will have had their blob downloaded yet, so look ahead and fetch the next 3 images
-	 */
+	// Pre-fetching logic
 	useEffect(() => {
 		for (let i = 1; i <= 3; i++) {
-			const nextIndex = currentIndex + i
-			if (nextIndex < images.length && !images[nextIndex].imageBlobUrl) {
-				downloadFile(images[nextIndex].id)
+			const nextIndex = (currentIndex + i) % shuffledImages.length
+			if (!shuffledImages[nextIndex]?.imageBlobUrl) {
+				downloadFile(shuffledImages[nextIndex]?.id)
 			}
 		}
-	}, [currentIndex])
+	}, [currentIndex, shuffledImages, downloadFile])
 
-	const currentImage = images[currentIndex]
+	const currentImage = shuffledImages[currentIndex]
 
 	return (
 		<div className='slideShowContainer'>
 			<div className='slideShowMain'>
-				{currentImage.imageBlobUrl ? <img src={currentImage.imageBlobUrl} /> : <i className="h1 mb-0 bi-arrow-repeat" />}
+				{currentImage?.imageBlobUrl ? <img src={currentImage.imageBlobUrl} /> : <i className="h1 mb-0 bi-arrow-repeat" />}
 			</div>
 		</div>
 	)
 }
 
-export default Slideshow
+export default ImageSlideshow
