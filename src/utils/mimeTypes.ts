5c7e1ee3ccdf353ed7cf87f8777864d8abfd47e1
GitBrent <gitbrent@users.noreply.github.com>
2023-12-02 20:52:54 -0600

    added `isMedia`

--- src/utils/mimeTypes.ts
+++ src/utils/mimeTypes.ts
@@ -5,4 +5,11 @@ export const isVideo = (item: IGapiFile): boolean => item.mimeType.startsWith('v
 
 export const isImage = (item: IGapiFile): boolean => item.mimeType.startsWith('image/')
 
+/**
+ * isVideo || isImage
+ * @param item
+ * @returns
+ */
+export const isMedia = (item: IGapiFile): boolean => isVideo(item) || isImage(item)
+
 export const isFolder = (item: IGapiFile): boolean => item.mimeType === 'application/vnd.google-apps.folder'
