804e09fead03fc789a2bfec47d6616c53e2e6460
GitBrent <gitbrent@users.noreply.github.com>
2024-11-03 11:11:28 -0600

    retain port from CRA

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// [BDE]: Use port 3000 like CRA as thats what google API is expecting
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 8080
+		port: 3000
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true, // Add this line to suppress warnings
+			},
+		},
 	}
 })
