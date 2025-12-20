c9e4b5e3b95175c373a25c22429ba40eaaae2ad0
Brent Ely <gitbrent@users.noreply.github.com>
2025-12-20 14:12:08 -0600

    updated version

--- src/App.props.ts
+++ src/App.props.ts
@@ -2,8 +2,8 @@
  * APP
  * @see [SampleImages](https://unsample.net/)
  */
-export const APP_BLD = '20251220-1345'
-export const APP_VER = '1.2.0'
+export const APP_BLD = '20251220-1410'
+export const APP_VER = '1.3.0-WIP'
 
 /** Debug mode - shows development-only UI/features */
 export const DEBUG = (() => {
